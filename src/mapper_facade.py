b93d7b3c391349e2216861ce6514f88be339b4b2
Jan Klhůfek <jan.klhufek@gmail.com>
2023-08-30 18:58:13 +0200

    Added support for dynamic mapper heuristic creation for timeloop mapper facade.

--- src/mapper_facade.py
+++ src/mapper_facade.py
@@ -9,6 +9,7 @@ import csv
 import shutil
 import threading
 import yaml
+import multiprocessing
 from typing import Tuple
 from construct_workloads.create_model import create_keras_model
 from construct_workloads.parse_model import parse_keras_model
@@ -31,13 +32,60 @@ class MapperFacade:
         architecture (str): Name of the architecture to be used along with its associated components and constraints.
     """
     def __init__(self, configs_rel_path: str = "timeloop_configs", architecture: str = "eyeriss") -> None:
+        self._architecture = architecture
+        self._mode = f"timeloop-mapper"
+        self._thread_id = threading.get_ident()
+
         self.configs_path = configs_rel_path
         self.arch = glob.glob(f"{self.configs_path}/architectures/{architecture}/*.yaml")[0]
         self.components = glob.glob(f"{self.configs_path}/architectures/{architecture}/components/*.yaml")
         self.constraints = glob.glob(f"{self.configs_path}/architectures/{architecture}/constraints/*.yaml")
-        self._thread_id = threading.get_ident()
 
-        self.mode = f"timeloop-mapper"
+    """Method to modify the mapper heuristic settings for the given settings.
+    
+    Args:
+        mapper_config (dict): Parsed yaml config containing the mapper heuristic settings.
+        heuristic (str): Name of the mapper heuristic to be used. Choices are `exhaustive`, `hybrid`, `linear` or `random`.
+        threads (str): Number of threads to be used by the mapper heuristics. Choices are `all` or integer number.
+        total_valid (int, optional): Specifies the number of valid mappings to be considered by the mapper heuristic. A value of 0 means that this criteria is not used for thread termination.
+        Lower values will result in a reduced search space proportional to the workload`s dimensionality. Defaults to 0.
+        log_all (bool): Flag to log all the mappings.
+    Returns:
+        dict: Modified mapper heuristic settings.    
+    """
+    def _modify_mapper_configs(self, mapper_config, heuristic, metrics, threads, total_valid, log_all):
+        mapper_config["mapper"]["out_prefix"] = f"{self._mode}_{self._thread_id}"
+        mapper_config["mapper"]["optimization-metrics"] = list(metrics) if metrics[1] else [metrics[0]]
+        mapper_config["mapper"]["search-size"] = int(total_valid / threads) if threads != "all" else int(total_valid / multiprocessing.cpu_count())
+        
+        if log_all:
+            mapper_config["mapper"]["log-oaves"] = True
+            mapper_config["mapper"]["log-suboptimal"] = True
+            mapper_config["mapper"]["log-all"] = True
+            mapper_config["mapper"]["log-stats"] = True
+        
+        if isinstance(threads, int):
+            mapper_config["mapper"]["num-threads"] = threads
+        
+        if heuristic == "exhaustive":
+            mapper_config["mapper"]["algorithm"] = "linear-pruned" 
+            mapper_config["mapper"]["victory-condition"] = 0
+            mapper_config["mapper"]["timeout"] = 0
+            # Remove the "max-permutations-per-if-visit" if it exists
+            mapper_config["mapper"].pop("max-permutations-per-if-visit", None)
+        else:
+            mapper_config["mapper"]["victory-condition"] = 500
+            mapper_config["mapper"]["max-permutations-per-if-visit"] = 16
+            mapper_config["mapper"]["timeout"] = 15000
+            
+            if heuristic == "random":
+                mapper_config["mapper"]["algorithm"] = "random-pruned"
+            elif heuristic == "linear":
+                mapper_config["mapper"]["algorithm"] = "linear-pruned"
+            elif heuristic == "hybrid":
+                mapper_config["mapper"]["algorithm"] = "hybrid"
+
+        return mapper_config
 
     """Method to run timeloop-mapper for a given workload (i.e. a CNN layer) and mapper heuristic settings.
 
@@ -47,22 +95,27 @@ class MapperFacade:
         heuristic (str, optional): Name of the mapper heuristic to be used. Choices are `exhaustive`, `hybrid`, `linear` or `random`. Defaults to "random".
         metrics (tuple, optional): Tuple of two metrics to be used for the mapper heuristic. Possible values are all six combinations of `energy`, `delay`, `lla`
         with an additional seventh option `edp`, leaving the second metric blank. Defaults to ("energy", "delay").
+        total_valid (int, optional): Specifies the number of valid mappings to be considered by the mapper heuristic. A value of 0 means that this criteria is not used for thread termination.
+        Lower values will result in a reduced search space proportional to the workload`s dimensionality. Defaults to 0.
+        out_dir (str, optional): Relative path to the output directory where the timeloop-mapper output files are stored. Defaults to "tmp_outputs".
+        log_all (bool, optional): Flag to log all the mappings. Defaults to False.
         verbose (bool, optional): Flag to print the timeloop-mapper output. Defaults to False.
         clean (bool, optional): Flag to delete the temporary files generated by timeloop-mapper. Defaults to True.
 
     Returns:
         dict: Dictionary containing the best mapping's HW parameters and total runtime of the timeloop-mapper call.
     """
-    def run_one_workload(self, workload: str, threads: str = "all", heuristic: str = "random", metrics: Tuple[str, str] = ("energy", "delay"), verbose: bool = False, clean: bool = True) -> dict:
-        metric_fname = "edp" if metrics[0] == "edp" else f"{metrics[0]}-{metrics[1]}"
-        mapper = glob.glob(f"{self.configs_path}/mapper_heuristics/{threads}_threads/{heuristic}/{metric_fname}*.yaml")[0]
+    def run_one_workload(self, workload: str, batch_size: int = 1, threads: object = "all", heuristic: str = "random", metrics: Tuple[str, str] = ("energy", "delay"), total_valid: int = 0, out_dir: str = "tmp_outputs", log_all: bool = False, verbose: bool = False, clean: bool = True) -> dict:
+        mapper = f"{self.configs_path}/mapper_heuristics/mapper.yaml"
 
         with open(mapper, "r") as map:
             try:
                 config_dict = yaml.safe_load(map)
             except yaml.YAMLError as e:
                 print(e)
-        config_dict["mapper"]["out_prefix"] = f"{self.mode}_{self._thread_id}"
+                sys.exit(1)
+        # Modify the mapper heuristic settings for the given settings
+        config_dict = self._modify_mapper_configs(config_dict, heuristic, metrics, threads, total_valid, log_all)
 
         # Write the modified YAML data to a temporary file
         modified_mapper = os.path.splitext(mapper)[0] + f"_{self._thread_id}.yaml"
@@ -70,20 +123,20 @@ class MapperFacade:
             yaml.dump(config_dict, modified_map)
 
         start_time = time.time()
-        tmp_dir = f"tmp_outputs_{self._thread_id}"
+        tmp_dir = f"{out_dir}_{self._thread_id}"
         if not os.path.exists(tmp_dir):
             os.makedirs(tmp_dir)
 
         # Running the timeloop-mapper for the given workload and chosen mapper heuristic settings
         if verbose:
-            subprocess.run([self.mode, self.arch] + self.components + self.constraints
+            subprocess.run([self._mode, self.arch] + self.components + self.constraints
                            + [modified_mapper, workload, "-o", tmp_dir], check=True)
         else:
-            subprocess.run([self.mode, self.arch] + self.components + self.constraints
+            subprocess.run([self._mode, self.arch] + self.components + self.constraints
                            + [modified_mapper, workload, "-o", tmp_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 
         # Reading the CSV file into a dictionary
-        with open(f"{tmp_dir}/{self.mode}_{self._thread_id}.stats.csv", "r") as f:
+        with open(f"{tmp_dir}/{self._mode}_{self._thread_id}.stats.csv", "r") as f:
             reader = csv.DictReader(f)
             result_dict = next(reader)
 
@@ -95,7 +148,7 @@ class MapperFacade:
         end_time = time.time()
         runtime = end_time - start_time
         # Return dictionary with the best found HW params and total mapper runtime
-        return {"Workload": workload.split("/")[-1].split(".")[0], "Optimized_metric_1": metrics[0], "Optimized_metric_2": metrics[1], **result_dict, "Runtime [s]": "{:.2f}".format(runtime)}
+        return {"HW": self._architecture, "Workload": workload.split("/")[-1].split(".")[0], "Batch_size": batch_size, "Optimized_metric_1": metrics[0], "Optimized_metric_2": metrics[1], **result_dict, "Runtime [s]": "{:.2f}".format(runtime)}
 
     """Method to run timeloop-mapper for all workloads (i.e. a CNN network's layers) in a given folder and mapper heuristic settings.
 
@@ -105,19 +158,23 @@ class MapperFacade:
         heuristic (str, optional): Name of the mapper heuristic to be used. Choices are `exhaustive`, `hybrid`, `linear` or `random`. Defaults to "random".
         metrics (tuple, optional): Tuple of two metrics to be used for the mapper heuristic. Possible values are all six combinations of `energy`, `delay`, `lla`
         with an additional seventh option `edp`, leaving the second metric blank. Defaults to ("energy", "delay").
+        total_valid (int, optional): Specifies the number of valid mappings to be considered by the mapper heuristic. A value of 0 means that this criteria is not used for thread termination.
+        Lower values will result in a reduced search space proportional to the workload`s dimensionality. Defaults to 0.
+        out_dir (str, optional): Relative path to the output directory where the timeloop-mapper output files are stored. Defaults to "tmp_outputs".
+        log_all (bool, optional): Flag to log all the mappings. Defaults to False.
         verbose (bool, optional): Flag to print the timeloop-mapper output. Defaults to False.
         clean (bool, optional): Flag to delete the temporary files generated by timeloop-mapper. Defaults to True.
 
     Returns:
         dict: Dictionary containing the best mappings HW parameters and total runtime of the individual workloads timeloop-mapper calls.
     """
-    def run_all_workloads(self, workloads: str, threads: str = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), verbose: bool = False, clean: bool = True) -> dict:
+    def run_all_workloads(self, workloads: str, batch_size: int = 1, threads: object = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), total_valid: int = 0, out_dir: str = "tmp_outputs", log_all: bool = False, verbose: bool = False, clean: bool = True) -> dict:
         workloads = glob.glob(f"{workloads}/*.yaml")
         hw_params = {}
 
         # Retrieve parameters for each workload
         for i, workload in enumerate(workloads):
-            hw_params[workload.split("/")[-1].split(".")[0]] = self.run_one_workload(workload=workload, threads=threads, heuristic=heuristic, metrics=metrics, verbose=verbose, clean=clean)
+            hw_params[workload.split("/")[-1].split(".")[0]] = self.run_one_workload(workload=workload, batch_size=batch_size, threads=threads, heuristic=heuristic, metrics=metrics, total_valid=total_valid, out_dir=f"{out_dir}/{workload.split('/')[-1].split('.')[0]}", log_all=log_all, verbose=verbose, clean=clean)
             print("Finished workload ", i+1, "/", len(workloads))
 
         # Return dictionary with individual workload's HW params and runtime
@@ -149,13 +206,17 @@ class MapperFacade:
         heuristic (str, optional): Name of the mapper heuristic to be used. Choices are `exhaustive`, `hybrid`, `linear` or `random`. Defaults to "random".
         metrics (tuple, optional): Tuple of two metrics to be used for the mapper heuristic. Possible values are all six combinations of `energy`, `delay`, `lla`
         with an additional seventh option `edp`, leaving the second metric blank. Defaults to ("energy", "delay").
+        total_valid (int, optional): Specifies the number of valid mappings to be considered by the mapper heuristic. A value of 0 means that this criteria is not used for thread termination.
+        Lower values will result in a reduced search space proportional to the workload`s dimensionality. Defaults to 0.
+        out_dir (str, optional): Relative path to the output directory where the timeloop-mapper output files are stored. Defaults to "tmp_outputs".
+        log_all (bool, optional): Flag to log all the mappings. Defaults to False.
         verbose (bool, optional): Flag to print the timeloop-mapper output. Defaults to False.
         clean (bool, optional): Flag to delete the temporary files generated by timeloop-mapper. Defaults to True.
 
     Returns:
         dict: Dictionary containing the best mappings HW parameters and total runtime of the individual workloads of the given cnn model.
     """
-    def get_hw_params_create_model(self, api_choice: str, model: str, batch_size: int = 1, bitwidths: object = None, input_size: str = "224,224,3", threads: str = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), verbose: bool = False, clean: bool = True) -> dict:
+    def get_hw_params_create_model(self, api_choice: str, model: str, batch_size: int = 1, bitwidths: object = None, input_size: str = "224,224,3", threads: object = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), total_valid: int = 0, out_dir: str = "tmp_outputs", log_all: bool = False, verbose: bool = False, clean: bool = True) -> dict:
         if isinstance(input_size, str):
             input_size = tuple((int(d) for d in str.split(input_size, ",")))
         # Create templates for individual model's CONV layers
@@ -183,7 +244,7 @@ class MapperFacade:
             sys.exit(0)
 
         # Run timeloop-mapper on the created workloads
-        results = self.run_all_workloads(workloads=workloads_location, threads=threads, heuristic=heuristic, metrics=metrics, verbose=verbose, clean=clean)
+        results = self.run_all_workloads(workloads=workloads_location, batch_size=batch_size, threads=threads, heuristic=heuristic, metrics=metrics, total_valid=total_valid, out_dir=out_dir, log_all=log_all, verbose=verbose, clean=clean)
         return results
 
     """Method to parse a cnn model and run timeloop-mapper for all workloads (i.e. a CNN network's layers) for given configuration and mapper heuristic settings.
@@ -201,13 +262,17 @@ class MapperFacade:
         heuristic (str, optional): Name of the mapper heuristic to be used. Choices are `exhaustive`, `hybrid`, `linear` or `random`. Defaults to "random".
         metrics (tuple, optional): Tuple of two metrics to be used for the mapper heuristic. Possible values are all six combinations of `energy`, `delay`, `lla`
         with an additional seventh option `edp`, leaving the second metric blank. Defaults to ("energy", "delay").
+        total_valid (int, optional): Specifies the number of valid mappings to be considered by the mapper heuristic. A value of 0 means that this criteria is not used for thread termination.
+        Lower values will result in a reduced search space proportional to the workload`s dimensionality. Defaults to 0.
+        out_dir (str, optional): Relative path to the output directory where the timeloop-mapper output files are stored. Defaults to "tmp_outputs".
+        log_all (bool, optional): Flag to log all the mappings. Defaults to False.
         verbose (bool, optional): Flag to print the timeloop-mapper output. Defaults to False.
         clean (bool, optional): Flag to delete the temporary files generated by timeloop-mapper. Defaults to True.
 
     Returns:
         dict: Dictionary containing the best mappings HW parameters and total runtime of the individual workloads of the given cnn model.
     """
-    def get_hw_params_parse_model(self, model: str, batch_size: int = 1, bitwidths: object = None, input_size: str = "224,224,3", threads: str = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), verbose: bool = False, clean: bool = True) -> dict:
+    def get_hw_params_parse_model(self, model: str, batch_size: int = 1, bitwidths: object = None, input_size: str = "224,224,3", threads: object = "all", heuristic: str = "random", metrics: tuple = ("energy", "delay"), total_valid: int = 0, out_dir: str = "tmp_outputs", log_all: bool = False, verbose: bool = False, clean: bool = True) -> dict:
         if not os.path.exists(model):
             raise FileNotFoundError(f"No model file '{model}' found.")
 
@@ -242,7 +307,7 @@ class MapperFacade:
             sys.exit(0)
 
         # Run timeloop-mapper on the created workloads
-        results = self.run_all_workloads(workloads=workloads_location, threads=threads, heuristic=heuristic, metrics=metrics, verbose=verbose, clean=clean)
+        results = self.run_all_workloads(workloads=workloads_location, batch_size=batch_size, threads=threads, heuristic=heuristic, metrics=metrics, total_valid=total_valid, out_dir=out_dir, log_all=log_all, verbose=verbose, clean=clean)
         return results
 
 
