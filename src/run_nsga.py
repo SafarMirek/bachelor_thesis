d753ad98a4df71ca9750242e098da9536ac00c02
Miroslav Šafář <miroslav.safar@znojmo.cz>
2023-09-14 08:08:36 +0200

    fix: missing bn_freeze param

--- src/run_nsga.py
+++ src/run_nsga.py
@@ -12,7 +12,8 @@ from nsga.nsga_qat_multigpu import MultiGPUQATNSGA
 
 def main(*, logs_dir, base_model_path, parent_size=25, offspring_size=25, batch_size=64, qat_epochs=6, generations=25,
          previous_run=None, cache_datasets=False, approx=False, act_quant_wait=0, per_channel=True, symmetric=True,
-         learning_rate=0.2, multigpu=False, exhaustive=False, timeloop_architecture="eyeriss", model_name="mobilenet"):
+         learning_rate=0.2, multigpu=False, exhaustive=False, timeloop_architecture="eyeriss", model_name="mobilenet",
+         bn_freeze=40):
     """
     Run NSGA-II for searching of best mixed-precision quantization configurations of layers
 
@@ -47,7 +48,7 @@ def main(*, logs_dir, base_model_path, parent_size=25, offspring_size=25, batch_
                                cache_datasets=cache_datasets, approx=approx, activation_quant_wait=act_quant_wait,
                                per_channel=per_channel, symmetric=symmetric, learning_rate=learning_rate,
                                timeloop_heuristic=timeloop_heuristic, timeloop_architecture=timeloop_architecture,
-                               model_name=model_name)
+                               model_name=model_name, bn_freeze=bn_freeze)
     else:
         print(f"Initializing QAT NSGA-II for {timeloop_architecture} architecture")
         nsga = QATNSGA(logs_dir=logs_dir, base_model_path=base_model_path, parent_size=parent_size,
@@ -57,7 +58,7 @@ def main(*, logs_dir, base_model_path, parent_size=25, offspring_size=25, batch_
                        cache_datasets=cache_datasets, approx=approx, activation_quant_wait=act_quant_wait,
                        per_channel=per_channel, symmetric=symmetric, learning_rate=learning_rate,
                        timeloop_heuristic=timeloop_heuristic, timeloop_architecture=timeloop_architecture,
-                       model_name=model_name)
+                       model_name=model_name, bn_freeze=bn_freeze)
 
     nsga.run()
 
